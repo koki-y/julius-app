7f471e25524924a18d441720b2bd1ad85f72e1ad
mouja-gog <mouja-gog@gmail.com>
2018-09-28 09:56:09 +0900

    色々修正

--- scripts/julius-start.sh
+++ scripts/julius-start.sh
@@ -1,3 +1,3 @@
-julius -C ~/dictation-kit-dictation-kit-v4.3.1/main.jconf -C ~/dictation-kit-dictation-kit-v4.3.1/am-gmm.jconf -module > /dev/null &
+julius -C ~/dictation-kit-dictation-kit-v4.3.1/main.jconf -C ~/dictation-kit-dictation-kit-v4.3.1/am-gmm.jconf -module < /dev/null > /dev/null 2> /dev/null &
 echo $!
 sleep 2
--- src/python/receiver.py
+++ src/python/receiver.py
@@ -1,58 +1,50 @@
 import os
+import threading
 import xml.etree.ElementTree as ET
 import julius_conf as conf
 import julius_utils as utils
 
-opsQueue = []
-receiving = False
-
 def main():
 	client = utils.makeConnection()
+
+	receive_and_print = threading.Thread(target=receiving_julius_msg_and_print, args=[client])
 	try:
-		data = ''
-		while 1:
-			if opsQueue:
-				if not receiving:
-					ops = opsQueue.pop()
-					print('#### send message.')
-					message = ops.encode('utf-8')
-					client.send(message)
-					receiving = True
-			else:
-				data += str(client.recv(1024).decode('utf-8'))
-				if conf.escapeChar in data:
-					splitedDot = data.split(conf.escapeChar)
-					processOnReceive(splitedDot[0:-1])
-					data = ''.join(splitedDot[-1:])
-					receiving = False
+		receive_and_print.start()
+		wait_stdin_and_send_command(client)
 	except KeyboardInterrupt:
 		client.close()
 
-def processOnReceive(datas):
+def wait_stdin_and_send_command(client):
+	while True:
+		message = input()
+		message = message.upper()
+		print('[PROC_MSG] SND: "'+ message + '"')
+		message += os.linesep
+		client.send(message.encode('utf-8'))
+
+def receiving_julius_msg_and_print(client):
+	data = ''
+	while True:
+		data += str(client.recv(1024).decode('utf-8'))
+		if conf.escapeChar in data:
+			splitedDot = data.split(conf.escapeChar)
+			onReceiveMessage(splitedDot[0:-1])
+			data = ''.join(splitedDot[-1:])
+	
+def onReceiveMessage(datas):
 	for data in datas:
 		xmldata = perse_xml(remove_gabage(data))
 		print_xml(xmldata)
-
-		# command送信テスト部分 (状態が整ったらQueueに置く)
-		if xmldata.tag == 'INPUT':
-			if xmldata.attrib['STATUS'] == 'LISTEN':
-				print('#### is INPUT.LISTEN')
-				opsQueue.append('STATUS' + os.linesep)
-				# opsQueue.append('VERSION' + os.linesep)
-				# opsQueue.append('GRAMINFO' + os.linesep)
-				# 以下のコマンドはパースエラーが出る
-				# opsQueue.append('LISTPROCESS' + os.linesep)
 
 def perse_xml(data):
 	return ET.fromstring(data)
 
 def print_xml(xml_data):
-	print(xml_data.tag, xml_data.attrib)
+	print('> ', xml_data.tag, xml_data.attrib)
 	for child in xml_data:
-		print('  ', child.tag, child.attrib)
+		print('>   ', child.tag, child.attrib)
 		for mago in child:
-			print('    ', mago.tag, mago.attrib)
-	print()
+			print('>     ', mago.tag, mago.attrib)
 
 def remove_gabage(data):
 	for gabage in conf.gabages:
